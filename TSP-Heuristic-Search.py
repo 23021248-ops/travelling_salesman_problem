4996cee72d1d2feb6947a2327694d3b7bdf99f2b
Đức <23021248@vnu.edu.vn>
2026-06-09 10:57:56 +0700

    Initial commit

---
+++ TSP-Heuristic-Search.py
@@ -0,0 +1,79 @@
+import heapq
+
+dist = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+
+n = len(dist)
+
+def heuristic(unvisited):
+    if not unvisited:
+        return 0
+
+    h = 0
+
+    for city in unvisited:
+        h += min(
+            dist[city][j]
+            for j in range(n)
+            if j != city
+        )
+
+    return h / 2
+
+
+pq = []
+
+start = 0
+
+heapq.heappush(
+    pq,
+    (0, 0, [start], set(range(1, n)))
+)
+
+best_cost = float('inf')
+best_path = None
+
+while pq:
+
+    f, g, path, unvisited = heapq.heappop(pq)
+
+    current = path[-1]
+
+    if not unvisited:
+
+        total_cost = g + dist[current][start]
+
+        if total_cost < best_cost:
+            best_cost = total_cost
+            best_path = path + [start]
+
+        continue
+
+    for city in unvisited:
+
+        new_g = g + dist[current][city]
+
+        new_unvisited = unvisited.copy()
+        new_unvisited.remove(city)
+
+        h = heuristic(new_unvisited)
+
+        heapq.heappush(
+            pq,
+            (
+                new_g + h,
+                new_g,
+                path + [city],
+                new_unvisited
+            )
+        )
+
+print("Best Tour:")
+print(best_path)
+
+print("Total Cost:")
+print(best_cost)
